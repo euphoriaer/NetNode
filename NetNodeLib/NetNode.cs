e4d4b60d86ebc0e793aec722fa772d25ea1beb5e
panbin <1764183867@qq.com>
2023-08-21 15:47:44 +0800

    Update Node

--- NetNodeLib/NetNode.cs
+++ NetNodeLib/NetNode.cs
@@ -1,15 +1,56 @@
-﻿namespace NetNodeLib
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNodeLib
 {
-    public partial class NetNode : UserControl
+    public  class NetNode
     {
-        public NetNode()
+        public string Name;
+
+        private bool _IsSelected;
+
+        public Vector2 Position=new Vector2(0,0);
+
+        public int Left;
+
+        public int Right;
+
+        public int Top;
+
+        public int Bottom;
+
+        public List<NodeDot> LeftDots;
+
+        public List<NodeDot> RightDots;
+
+        public void ConnectNodeDot(NodeDot dot1,NodeDot dot2)
         {
-            InitializeComponent();
+            //连接 1,2
         }
 
-        private void NetNode_Load(object sender, EventArgs e)
+        public void DrawDot()
         {
-            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
         }
+
+        public void DrawTitle()
+        {
+
+        }
+
+        public void DrawNode()
+        {
+
+        }
+
+        public void DrawConnectLine()
+        {
+
+        }
+
     }
 }
---
+++ NetNodeLib/NetNodeEditor.cs
@@ -0,0 +1,23 @@
+﻿namespace NetNodeLib
+{
+    public partial class NetNodeEditor : UserControl
+    {
+        public NetNodeEditor()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void NetNode_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void CreateNode(NetNode node)
+        {
+
+        }
+
+
+    }
+}
